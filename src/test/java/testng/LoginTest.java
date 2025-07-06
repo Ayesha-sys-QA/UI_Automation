72b733d304146c92880fd59db7ebb93fed389a47
Ayesha <aicha.moustatia20@gmail.com>
2025-07-06 12:46:35 +0100

    Create LoginTest.java

---
+++ src/test/java/testng/LoginTest.java
@@ -0,0 +1,10 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void testLoginExample() {
+        System.out.println("TestNG test lancé avec succès !");
+    }
+}
